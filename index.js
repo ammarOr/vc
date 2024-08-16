76765f425de851d14bb6bd1540d76e0d994460bf
ammarOr <119805547+ammarOr@users.noreply.github.com>
2024-08-16 16:40:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,18 @@ app.use('/ping', (req, res) => {
 const { Client } = require('discord.js-selfbot-v13');
 const client = new Client(); // All partials are loaded automatically
 ///By IC丶SNOW
+const express = require("express")
+const app = express();
+var listener = app.listen(process.env.PORT || 2000, function () {
+  console.log(Your app is listening on port ' + listener.address().port);
+});
+app.listen(() => console.log("I'm Ready To Work..! 24H");
+app.get('/', (req, res) => {
+  res.send('
+  <body>
+  <center><h1>Bot 24H ON!</h1></center 
+  </body>')
+});
 client.on('ready', async () => {
   console.log(`${client.user.username} is ready!                        Fatures: 
 1- Streaming Added
